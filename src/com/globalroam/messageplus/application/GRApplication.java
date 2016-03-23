187a8d7545ad7747896c8ab1c94ffb0d6635ef62
chenlu <413162035@qq.com>
2016-03-23 23:12:12 +0800

    add appid for xinsina

--- src/com/globalroam/messageplus/application/GRApplication.java
+++ src/com/globalroam/messageplus/application/GRApplication.java
@@ -33,7 +33,7 @@ public class GRApplication extends Application {
 	private void SocialSDKInit() {
 		PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
 		// 微信 appid appsecret
-		PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad");
+		PlatformConfig.setSinaWeibo("624321006", "98f6becfda60b5c688f74b21b6b51405");// 已更换
 		// 新浪微博 appkey appsecret
 		PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
 		// QQ和Qzone appid appkey
